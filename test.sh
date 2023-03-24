669c06877c2b22a7238b6a2c783bfb6ea1c99a04
Satoshi Kataoka <k.3104is@gmail.com>
2023-03-24 12:30:30 +0900

    add --verifyudev

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 # make encrypted img
 echo "### make encrypted img ###"
 sudo dd if=/dev/zero of=./encrypted.img bs=1M count=32
@@ -7,7 +9,7 @@ LOOP_DEV=$(losetup -f)
 sudo losetup ${LOOP_DEV} ./encrypted.img
 #sudo mkfs -t ext4 -O has_journal ${LOOP_DEV}
 #sudo dmsetup -v create encrypted --table "0 $(sudo blockdev --getsize ${LOOP_DEV}) crypt capi:cbc(aes)-plain :36:logon:logkey: 0 ${LOOP_DEV} 0 1 sector_size:512"
-sudo dmsetup -v create encrypted --table "0 $(sudo blockdev --getsize /dev/loop12) crypt aes-cbc-essiv:sha256 babebabebabebabebabebabebabebabe 0 /dev/loop12 0"
+sudo dmsetup -v create encrypted --table "0 $(sudo blockdev --getsize ${LOOP_DEV}) crypt aes-cbc-essiv:sha256 babebabebabebabebabebabebabebabe 0 ${LOOP_DEV} 0" --verifyudev
 sudo dmsetup table --showkey encrypted
 
 # mount, write and remove
@@ -22,7 +24,7 @@ sudo losetup -d ${LOOP_DEV}
 # restart and check
 echo "### restart and check ###"
 sudo losetup ${LOOP_DEV} ./encrypted.img
-sudo dmsetup -v create encrypted --table "0 $(sudo blockdev --getsize /dev/loop12) crypt aes-cbc-essiv:sha256 babebabebabebabebabebabebabebabe 0 /dev/loop12 0"
+sudo dmsetup -v create encrypted --table "0 $(sudo blockdev --getsize ${LOOP_DEV}) crypt aes-cbc-essiv:sha256 babebabebabebabebabebabebabebabe 0 ${LOOP_DEV} 0" --verifyudev
 sudo mount -t ext4 /dev/mapper/encrypted /mnt
 sudo cat /mnt/readme.txt
 
@@ -32,3 +34,5 @@ sudo umount /mnt
 sudo dmsetup remove encrypted
 sudo losetup -d ${LOOP_DEV}
 sudo rm -f ./encrypted.img
+
+set +x
